286945de148e5842e21a5924aa53f559de963f6d
Xugehui <1239671652@qq.com>
2021-09-28 17:49:36 +0800

    知识点3

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    num:0  
+    num:0 ,
+    list:[]
   },
   handleInput(e){
     //console.log(e.detail.value)
@@ -16,7 +17,38 @@ Page({
   handletap(e){
    console.log(e)
    
+  },
+  //-------------------------------1\监听页面的生命周期函数---------------
+  onLoad(){
+    console.log('onload:生命周期回调—监听页面加载')
+    wx.request({
+      url: 'http://123.207.32.32:8000/recommend',
+      success:function(res){
+        console.log(res)
+        const data = res.data
+        console.log(data)
+        this.setData({
+          list: data
+        }) 
+
+      }
+    })
+  },
+  //页面初次渲染完成时会回调
+  onReady(){
+    console.log('onready:生命周期回调—监听页面初次渲染完成')
+  },
+  onHide(){
+    console.log('onhide:生命周期回调—监听页面隐藏')
+  },
+  //页面显示出来时
+  onShow(){
+    console.log('onshow:生命周期回调—监听页面显示')
+  },
+  onUnload(){
+    console.log('onUnLoad:生命周期回调—监听页面卸载')
   }
+
 
   
 })
