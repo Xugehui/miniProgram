9a07c2181d0eaf0d60bc0e756d357f9283b02364
Xugehui <1239671652@qq.com>
2021-09-28 16:06:57 +0800

    知识点2

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
 // app.js
+//注册一个小程序示例
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  //生命周期函数：后台存活2小时
+  onLaunch:function (){
+    console.log('小程序初始化完成：onlauch')
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    //异步调用-
+    // wx.getUserInfo({
+    //   //数据拿到之后，在进行回调
+    //   success: function(res){
+    //     console.log(res)
+    //   }
+    // })
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+
   },
+  //小程序界面显示出来之后会执行的生命周期函数
+  onShow:function(options){
+    console.log('这里是onshow')
+    //console.log(options)
+    switch(options.scene){
+      //判断小程序的进入场景
+      case 1001:
+        break;
+      case 1002:
+        break;
+    }
+  },
+  onHide:function(){
+    console.log('界面被隐藏是执行：onHide')
+  },
+  onError:function(msg){
+    console.log('小程序中发生错误时会执行')
+  },
+  // onLaunch() {
+    
+  //   // 展示本地存储能力
+  //   const logs = wx.getStorageSync('logs') || []
+  //   logs.unshift(Date.now())
+  //   wx.setStorageSync('logs', logs)
+
+  //   // 登录
+  //   wx.login({
+  //     success: res => {
+  //       // 发送 res.code 到后台换取 openId, sessionKey, unionId
+  //     }
+  //   })
+  // },
+  //全局均可用
+  //具体用法 详见home.js
   globalData: {
-    userInfo: null
+    name:'小名',
+    age:18
+    
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,11 +1,19 @@
 // pages/home/home.js
+
+//getAPP()获取App()产生的示例对象
+const app = getApp()
+console.log(app.globalData.name)
+const name = app.globalData.name
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    num:0  
+    num:0,
+    userInfo: {},
+    hasUserInfo: false,
+    canIUseGetUserProfile: false,
   },
   handleInput(e){
     //console.log(e.detail.value)
@@ -16,7 +24,30 @@ Page({
   handletap(e){
    console.log(e)
    
-  }
+  },
+  handleGetuserinfo(event){
+    console.log(event)
+  },
+  onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
 
   
 })
